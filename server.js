f3f27e199f804dd0dbd0675444aeb7ccc9725eb8
Sarah0079ja <sodiavbara@gmail.com>
2020-03-31 13:53:51 +0100

    installedcors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require("config");
+const cors = require("cors")
 const path = require('path')
 
 const app = express(); 
 
 //middleware bodyparser
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
